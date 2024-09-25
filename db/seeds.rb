12723b354d778b78be0005697a4e030c15364cd1
Shanshan Fu <jesse.fu21@gmail.com>
2024-09-25 23:25:20 +0100

    yelp

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,19 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Restaurant.destroy_all
+puts "Creating restaurants......"
+
+restaurants = [
+  { name: "Sushi Samba", address: "123 Tokyo St, Japan", phone_number: "123-456-7890", category: "japanese" },
+  { name: "Pizza Napoli", address: "456 Rome St, Italy", phone_number: "987-654-3210", category: "italian" },
+  { name: "Le Bistro", address: "789 Paris St, France", phone_number: "456-789-0123", category: "french" },
+  { name: "Beijing Duck House", address: "101 Beijing St, China", phone_number: "321-654-9870", category: "chinese" },
+  { name: "Belgian Waffles", address: "102 Brussels St, Belgium", phone_number: "654-321-0987", category: "belgian" }
+]
+
+restaurants.each do |restaurant|
+  Restaurant.create(restaurant)
+end
+
+puts "Restaurants installed!"
